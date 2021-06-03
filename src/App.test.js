7344614e452a1917cc6085085aaf366866b6cd81
Andre Cortes <andre.crts@gmail.com>
2021-06-03 12:34:59 -0600

    add content to webpage

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/software avanzado/i);
+  expect(titleElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/ricardo andre/i);
+  expect(nameElement).toBeInTheDocument();
+  const carneElement = screen.getByText(/200915461/i);
+  expect(carneElement).toBeInTheDocument();
 });
